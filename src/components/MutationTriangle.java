f48ab1bb69e8c8f4575f6a4a695f6f89506909cc
t5Fab <fab.ambrogi@gmail.com>
2017-09-27 15:16:31 +0200

    added 2 mutation classes

---
+++ src/components/MutationTriangle.java
@@ -0,0 +1,33 @@
+package src.components;
+
+import src.AMutation;
+import src.Individual;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MutationTriangle extends AMutation {
+
+    private double max;
+    double mutationProbability;
+
+    public MutationTriangle(double max, double mutationProbability) {
+        this.max = max;
+        this.mutationProbability = mutationProbability;
+    }
+
+    public void mutate(ArrayList<Individual> individuals)
+    {
+        Random rng = new Random();
+
+        for(Individual individual:individuals) {
+            if (rng.nextDouble() < mutationProbability) {
+                double[] genome = individual.getGenome();
+                for (int i = 0; i < genome.length; i++) {
+                    genome[i] += ((rng.nextDouble() + rng.nextDouble()) * max) - max;
+                }
+                individual.setGenome(genome);
+            }
+        }
+    }
+}

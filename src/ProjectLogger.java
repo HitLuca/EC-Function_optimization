e89ca49f4e0c60defda5e57a36bc22608b561cb0
HitLuca <luca.simonetto.94@gmail.com>
2017-09-28 17:17:40 +0200

    updated project logger and python plotter

--- src/ProjectLogger.java
+++ src/ProjectLogger.java
@@ -1,6 +1,9 @@
 package src;
 
 import java.io.*;
+import java.nio.channels.FileChannel;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 
 public class ProjectLogger {
@@ -11,31 +14,71 @@ public class ProjectLogger {
         Random rng = new Random();
 
 //        String function = "BentCigarFunction";
-//        String function = "SchaffersEvaluation";
-        String function = "KaatsuraEvaluation";
+        String function = "SchaffersEvaluation";
+//        String function = "KatsuuraEvaluation";
 
-        String outputPath = "logs/output.log";
+        int runsNumber = 4;
 
-        FileWriter fileWriter = new FileWriter(outputPath);
-        logger = new BufferedWriter(fileWriter);
-        writeHeader();
+        Date date = new Date() ;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss") ;
 
-        Process pr = run.exec("mv ./testrun/EC-Project.jar ./testrun/submission.jar");
-        pr.waitFor();
+        String outputPath = "logs/" + dateFormat.format(date) + "_" + function + "/";
+        new File(outputPath).mkdir();
 
-        pr = run.exec("java -jar ./testrun/testrun.jar -submission=player27 -evaluation=" + function + " -seed=1");
-        pr.waitFor();
-        BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-        String line;
+        for (int i=0; i<runsNumber; i++) {
+            String filename = i + ".log";
 
-        while ((line = buf.readLine()) !=null )
-        {
-            System.out.println(line);
-            logger.write(line + "\n");
+            FileWriter fileWriter = new FileWriter(outputPath + filename);
+            logger = new BufferedWriter(fileWriter);
+
+            Process pr = run.exec("mv ./testrun/EC-Project.jar ./testrun/submission.jar");
+            pr.waitFor();
+
+            pr = run.exec("java -jar ./testrun/testrun.jar -submission=player27 -evaluation=" + function + " -seed=1");
+//            pr.waitFor();
+
+            BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+            String line;
+
+            while ((line = buf.readLine()) !=null )
+            {
+                System.out.println(line);
+                logger.write(line + "\n");
+            }
+            logger.close();
+
+            File source=new File(outputPath + filename);
+            File destination=new File("logs/" + "last_run.log");
+
+            copyFile(source,destination);
         }
-        logger.close();
+
+
     }
 
-    private static void writeHeader() throws IOException {
+    public static void copyFile(File sourceFile, File destFile) throws IOException {
+        if(!destFile.exists()) {
+            destFile.createNewFile();
+        }
+
+        FileChannel source = null;
+        FileChannel destination = null;
+        try {
+            source = new RandomAccessFile(sourceFile,"rw").getChannel();
+            destination = new RandomAccessFile(destFile,"rw").getChannel();
+
+            long position = 0;
+            long count    = source.size();
+
+            source.transferTo(position, count, destination);
+        }
+        finally {
+            if(source != null) {
+                source.close();
+            }
+            if(destination != null) {
+                destination.close();
+            }
+        }
     }
 }
